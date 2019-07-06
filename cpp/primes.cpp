b6e96dbfd3cb875a103b9c2568c62a54f6f756e6
carlosefrias <carlosefrias@gmail.com>
2019-07-06 21:25:27 +0100

    primes update

--- cpp/primes.cpp
+++ cpp/primes.cpp
@@ -15,7 +15,7 @@ bool is_prime(long number)
 {
     bool is_prime = true;
     long divisor = 2;
-    while(is_prime && divisor < number)
+    while(is_prime && divisor < int(number / 2) + 1)
     {
         is_prime = !(number % divisor == 0);
         divisor++;
@@ -25,12 +25,38 @@ bool is_prime(long number)
 
 long first_divisor(long number)
 {
-    for(int i = 2; i < number; i++)
+    for(int i = 2; i < int(number / 2) + 1; i++)
     {
         if (number % i == 0)
             return i;
     }
     return number;
+}
+
+long * get_all_divisors(long number)
+{
+    long* list;
+    list = (long*) malloc(number * sizeof(long));  //memory allocated using malloc
+    list[0] = 1;
+    list[1] = 1;
+    int pos = 2;
+    for(int i = 2; i < (int) (number / 2) + 1; i++)
+    {
+        if(number % i == 0)
+            list[pos++] = i;
+    }
+    list[pos++] = number;
+    list[0] = pos;
+    return list;
+}
+
+void display_divisors(long * list)
+{
+    long n = list[0];
+    for(long i = 1; i < n; i++)
+    {
+        cout << list[i] << endl;
+    }
 }
 
 int main()
@@ -61,6 +87,8 @@ int main()
             if(!prime)
             {
                 cout << "The smallest divisor of " << number << ", not counting 1, is " << first_divisor(number) << endl;
+                cout << "All divisors of " << number << " are: " << endl;
+                display_divisors(get_all_divisors(number));
             }
         }
     }
