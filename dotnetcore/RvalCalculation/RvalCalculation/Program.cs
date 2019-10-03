1f3ec52a08ac0c3bc4bd5513dad3ea5ec15aece9
Carlos Frias <carlosefrias@gmail.com>
2019-10-03 08:50:56 +0100

    tyding up using LINQ

--- dotnetcore/RvalCalculation/RvalCalculation/Program.cs
+++ dotnetcore/RvalCalculation/RvalCalculation/Program.cs
@@ -7,12 +7,13 @@ namespace RvalCalculation
     {
         static void Main(string[] args)
         {
-            var phi1A = 4.0;
-            var phi2A = 4.0; 
-            var phi3A = 1.0;
-            var phi1B = 21.0;
-            var phi2B = 21.0; 
-            var phi3B = 24.0;
+            var phi1A = 2.82;
+            var phi2A = 4.07; 
+            var phi3A = 2.45;
+            
+            var phi1B = 4.34;
+            var phi2B = 1.67; 
+            var phi3B = 38;
             var values = new RValCalculationValues
             {
                 Phi1A = phi1A,
@@ -25,29 +26,6 @@ namespace RvalCalculation
             var rValCalculator = new RValCalculator();
             var rVal = rValCalculator.CalculateRVal(values);
             Console.WriteLine($"rVal: {rVal}");
-            
-            
-            var a = DenseMatrix.OfArray(new[,]
-            {
-                { 1, 2, 3 },
-                { 4, 5, 6 },
-                { 7, 8, 9.0 }
-            });
-            
-            var b = DenseMatrix.OfArray(new[,]
-            {
-                { 11, 22, 53 },
-                { 4, 54, 60 },
-                { 1, 7, -2.0 }
-            });
-
-            var x = a * b;
-
-//            var v1 = DenseVector.OfArray(new[] { 1.0, -21.3, 34.6});
-//            var v2 = DenseVector.OfArray(new[] { 11.0, 56.3, 9.6});
-//            var aNew = rValCalculator.Angle(v1, v2);
-//            var aOld = rValCalculator.AngleOld(v1, v2);
-//            Console.WriteLine($"new: {aNew}\nold:{aOld}");
         }
     }
 }
--- dotnetcore/RvalCalculation/RvalCalculation/RValCalculator.cs
+++ dotnetcore/RvalCalculation/RvalCalculation/RValCalculator.cs
@@ -7,21 +7,38 @@ public class RValCalculator : IRValCalculator
 {
     private const double Tolerance = 1.0E-10;
     // define normals to all 001 surfaces
-    private static readonly DenseMatrix SurfaceNormals = DenseMatrix.OfArray( new double[ , ]
+    private static readonly List<DenseVector> SurfaceNormals = new List<DenseVector>
     {
-        { 1, 0, 0 },
-        { 0, 1, 0 },
-        { 0, 0, 1 },
-        { -1, 0, 0 },
-        { 0, -1, 0 },
-        { 0, 0, -1 }
-    } );
+        DenseVector.OfArray(new[] { 1.0, 0.0, 0.0 }),
+        DenseVector.OfArray(new[] { 0.0, 1.0, 0.0 }),
+        DenseVector.OfArray(new[] { 1.0, 0.0, 1.0 }),
+        DenseVector.OfArray(new[] { -1.0, 0.0, 0.0 }),
+        DenseVector.OfArray(new[] { 0.0, -1.0, 0.0 }),
+        DenseVector.OfArray(new[] { 0.0, 0.0, -1.0 }),
+    };
 
-
+    /// <summary>
+    /// Calculates the RVal between two crystal orientations
+    /// </summary>
+    /// <param name="rvalues"></param>
+    /// <returns></returns>
     public double CalculateRVal( RValCalculationValues rvalues )
     {
-        var (a, b) = GenerateAbList(rvalues);
+        // calculate orientation for primary grain from phi1 phi2 phi3
+        var psiA1 = rvalues.Phi1A.ToRadians();
+        var psiA2 = rvalues.Phi2A.ToRadians();
+        var psiA3 = rvalues.Phi3A.ToRadians();
 
+        var psiB1 = rvalues.Phi1B.ToRadians();
+        var psiB2 = rvalues.Phi2B.ToRadians();
+        var psiB3 = rvalues.Phi3B.ToRadians();
+        
+        var rotMatA = RotationMatrix( psiA1, psiA2, psiA3 );
+        var rotMatB = RotationMatrix( psiB1, psiB2, psiB3 );
+        
+        var a = SurfaceNormals.Select(sn => rotMatA * sn);
+        var b = SurfaceNormals.Select(sn => rotMatB * sn);
+        
         // calculate the angle between all possible <001> normals for both A and B grains
         // and find the minimum rotation along with the locations of these components in the vectors
         var angleTagPair = (from angleA in a
@@ -36,96 +53,56 @@ public class RValCalculator : IRValCalculator
             .OrderBy(val => val.initialAngle)
             .FirstOrDefault();
         if (angleTagPair == null)
-            return 0;
+            return 0.0;
         var angle = angleTagPair.initialAngle;
         var minAxes = angleTagPair.tag;
         
-        if ( Math.Abs(angle) < Tolerance || Math.Abs(angle - Math.PI / 2) < Tolerance )
-        {
-            return 0;
-        }
+        if (Math.Abs(angle) < Tolerance || Math.Abs(angle - Math.PI / 2) < Tolerance)
+            return 0.0;
 
         // calculate rotation axis to rotate B onto A using phi
         var axis = CrossProduct( minAxes[ 1 ], minAxes[ 0 ] );
 
         // perform rotation of grain B <001> normals onto A along defined axis and phi!
-
-        var rot = RotationAroundAxis( axis, angle );
-        Console.WriteLine($"rot: {rot}");
+        var rot = RotationAroundAxis(axis, angle);
+        var bRotated = b.Select(value => DenseVector.OfVector(rot * value));
         
-        var bRotated = b.Select(value => DenseVector.OfVector(rot * value)).ToList();
-        Console.WriteLine($"bRotated[0]: {bRotated[0]}");
         // Repeat procedure using the new orientation of grain B to calculate tau
-        var (tauAngles, tauTags) = GenerateTauAngles( a, bRotated );
-        Console.WriteLine($"tauAngles[0]: {tauAngles[0]}\ntauTags[0]: {tauTags[0]}");
-        
         // find the minimum rotation along with the locations of these components in the vectors
-        var d = tauAngles.Where(t => !double.IsNaN(t) && Math.Abs(t) > Tolerance ).OrderBy(t => t).FirstOrDefault();
-        Console.WriteLine($"d: {d}");
+        var tau = (from angleA in a
+                    from angleB in bRotated
+                    let angleBetweenThem = Angle(angleA, angleB)
+                    where !double.IsNaN(angleBetweenThem) && Math.Abs(angleBetweenThem) > Tolerance
+                    select angleBetweenThem)
+                .OrderBy(val => val)
+                .FirstOrDefault();
         
-        if ( Math.Abs(d) < Tolerance || Math.Abs(d - Math.PI / 2) < Tolerance )
+        if ( Math.Abs(tau) < Tolerance || Math.Abs(Math.Round(tau, 8) - Math.Round(Math.PI / 2)) < Tolerance )
         {
             return 180 / Math.PI * RValCal( 0, angle );
         }
 
-        return 180 / Math.PI * RValCal( d, angle );
+        return 180 / Math.PI * RValCal( tau, angle );
     }
 
-    internal (List<double> tauAngles, List<DenseVector[]> tauTags) GenerateTauAngles( IEnumerable<DenseVector> a, IEnumerable<DenseVector> bRotated )
-    {
-        var tauAngles = new List<double>();
-        var tauTags = new List<DenseVector[]>();
-
-        foreach ( var angleA in a )
-        {
-            foreach ( var angleB in bRotated )
-            {
-                tauAngles.Add( Angle( angleA, angleB ) );
-                tauTags.Add( new[] { angleA, angleB } );
-            }
-        }
-
-        return ( tauAngles, tauTags );
-    }
-
-    internal (List<DenseVector> A, List<DenseVector> B) GenerateAbList( RValCalculationValues rvalues )
-    {
-        // calculate orientation for primary grain from phi1 phi2 phi3
-        var psiA1 = rvalues.Phi1A * Math.PI / 180.0;
-        var psiA2 = rvalues.Phi2A * Math.PI / 180.0;
-        var psiA3 = rvalues.Phi3A * Math.PI / 180.0;
-
-        var psiB1 = rvalues.Phi1B * Math.PI / 180.0;
-        var psiB2 = rvalues.Phi2B * Math.PI / 180.0;
-        var psiB3 = rvalues.Phi3B * Math.PI / 180.0;
-
-        var a = new List<DenseVector>();
-        var b = new List<DenseVector>();
-
-        var rotMatA = RotationMatrix( psiA1, psiA2, psiA3 );
-        var rotMatB = RotationMatrix( psiB1, psiB2, psiB3 );
-
-        for (var i = 0; i < SurfaceNormals.RowCount; i++ )
-        {
-            var values = DenseVector.OfVector(SurfaceNormals.Row(i));
-
-            // calculate < 001 > surface normals for grain A
-            var matMulA = rotMatA * values;
-            a.Add(matMulA);
-
-            // calculate < 001 > surface normals for grain B
-            var matMulB = rotMatB * values;
-            b.Add(matMulB);
-        }
-
-        return ( a, b );
-    }
-
+    /// <summary>
+    /// Performs the final RVal calculation
+    /// knowing the value of tau and phi
+    /// </summary>
+    /// <param name="tau"></param>
+    /// <param name="phi"></param>
+    /// <returns></returns>
     internal double RValCal( double tau, double phi )
     {
         return Math.Acos( Math.Cos( tau ) * Math.Cos( phi ) );
     }
 
+    /// <summary>
+    /// Calculates the cross product (or external product) between two vectors
+    /// </summary>
+    /// <param name="a"></param>
+    /// <param name="b"></param>
+    /// <returns></returns>
     internal DenseVector CrossProduct( DenseVector a, DenseVector b )
     {
         double[] crossProduct =
@@ -134,7 +111,6 @@ public class RValCalculator : IRValCalculator
             a[ 2 ] * b[ 0 ] - a[ 0 ] * b[ 2 ],
             a[ 0 ] * b[ 1 ] - a[ 1 ] * b[ 0 ]
         };
-
         return DenseVector.OfArray(crossProduct);
     }
 
@@ -167,52 +143,16 @@ public class RValCalculator : IRValCalculator
         });
         return rotationMatrixAlpha * rotationMatrixBeta * rotationMatrixGamma;
     }
-    private double Angle(DenseVector a, DenseVector b )
-    {
-        return Math.Acos(DotProduct(a, b) / (Norm(a) * Norm(b)));
-    }
-
-    private double DotProduct(DenseVector a, DenseVector b)
-    {
-        var v1 = new[] {a[0], a[1], a[2]};
-        var v2 = new[] {b[0], b[1], b[2]};
-        return v1[0] * v2[0] + v1[1] * v2[1] + v1[2] * v2[2];
-    }
     
-    private double Norm(DenseVector a)
-    {
-        var v1 = new[] {a[0], a[1], a[2]};
-        return Math.Sqrt(v1[0] * v1[0] + v1[1] * v1[1] + v1[2] * v1[2]);
-    }
-
     /// <summary>
-    /// Return the minimum element of sequence and its corresponding label which is none zero and not a NAN
+    /// Calculates the angle (in radians) between two vectors
     /// </summary>
-    /// <param name="sequence"></param>
-    /// <param name="labels"></param>
-    internal (double angle, DenseVector[] minAxes) MinimumCalcNonZero( List<double> sequence, List<DenseVector[]> labels )
+    /// <param name="a"></param>
+    /// <param name="b"></param>
+    /// <returns></returns>
+    private double Angle(DenseVector a, DenseVector b)
     {
-        var minimumValue = sequence[ 0 ];
-        var minimumLabel = labels[ 0 ];
-
-        for (var i = 1; i < sequence.Count; i++)
-        {
-            var currentValue = sequence[ i ];
-            var currentLabel = labels[ i ];
-
-            if ( double.IsNaN( currentValue ) )
-            {
-                minimumValue = currentValue;
-                minimumLabel = currentLabel;
-            }
-            else if ((  currentValue < minimumValue || minimumValue == 0) && currentValue != 0)
-            {
-                minimumValue = currentValue;
-                minimumLabel = currentLabel;
-            }
-        }
-
-        return ( minimumValue, minimumLabel );
+        return Math.Acos(a.DotProduct(b) / (a.L2Norm() * b.L2Norm()));
     }
 
     /// <summary>
@@ -221,9 +161,9 @@ public class RValCalculator : IRValCalculator
     /// <param name="axis"></param>
     /// <param name="theta"></param>
     /// <returns></returns>
-    public DenseMatrix RotationAroundAxis( DenseVector axis, double theta )
+    private DenseMatrix RotationAroundAxis( DenseVector axis, double theta )
     {
-        var axisSquareRoot = axis / Math.Sqrt( DotProduct(axis, axis) );
+        var axisSquareRoot = axis / axis.L2Norm();
         var a = Math.Cos( theta / 2d );
 
         var vector2 = -axisSquareRoot * Math.Sin( theta / 2d );
@@ -249,5 +189,17 @@ public class RValCalculator : IRValCalculator
             { 2 * ( bd + ac ), 2 * ( cd - ab ), aa + dd - bb - cc },
         } );
     }
-
+}
+public static class AngleConverter
+{
+    /// <summary>
+    /// Extension method that allows to convert from degrees to radians
+    /// </summary>
+    /// <param name="angleInDegrees"></param>
+    /// <returns></returns>
+    public static double ToRadians(this double angleInDegrees)
+    {
+        // Angle in degrees
+        return (angleInDegrees * Math.PI) / 180.0;
+    }
 }
