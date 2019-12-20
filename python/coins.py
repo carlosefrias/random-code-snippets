d7c854d97de606f3740c0915b4143e758f0eded8
Carlos Frias <carlosefrias@gmail.com>
2019-12-20 21:58:45 +0000

    coins.py updated

--- python/coins.py
+++ python/coins.py
@@ -1,10 +1,10 @@
 coin_values = [200, 100, 50, 20, 10, 5, 2, 1]
-number_coins = [5, 10, 10, 20, 30, 40, 50, 50]
+number_coins = [5, 10, 10, 0, 30, 0, 50, 50]
 
 def get_available_amount():
     sum = 0
     for idx in range(len(coin_values)):
-        sum = sum + coin_values[idx] * number_coins[idx]
+        sum += coin_values[idx] * number_coins[idx]
     return sum
 
 def get_change(total_amount):
@@ -20,14 +20,12 @@ def get_change(total_amount):
             coins.append(value)
             number_coins[idx] -= 1
             sum_change += value
+        if sum_change == original_amount:
+            break
     if sum_change < original_amount:
         return "not enough money"
     return coins
 
-print(get_change(1050))
-print(get_change(1050))
-print(get_change(1050))
-print(get_change(255))
-print(get_change(145))
+print(get_change(25))
 
 print(f"available money: {get_available_amount()}")
