2313ee7f59e614225ec290df1e3292a7a6dbb1e7
Carlos Frias <carlos.frias@pvcomplete.com>
2021-07-18 16:47:44 +0100

    Uma versão optimizada da versão do C Paulo Freitas

---
+++ python/cres/CresDecres/CresDecresCPF/CresDecresCPF.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+int pow(int a, int n) {
+    int r = 1;
+    if (n > 0) {
+        for (int i = 0;i < n;i++) {
+            r *= a;
+        }
+    }
+    return r;
+}
+
+bool igual(int a, int b) {
+    return a == b;
+}
+
+
+int menor(int a, int b) {
+    return a < b;
+}
+
+int* numeroEmLista(int n) 
+{
+    static int lista[6];
+    for (int i = 5; i >= 0; i--) {
+        lista[i] = (n % pow(10, 6 - i)) / pow(10, 5 - i);
+    }
+    return lista;
+}
+
+int main()
+{
+    int c = 0, d = 0;
+
+    for (int i = 123456; i <= 987654; ++i) 
+    {
+        auto lista = numeroEmLista(i);
+        int s = 0;
+        auto breakOut = false;
+        for (int j = 0; j < 5; ++j) 
+        {
+            for (int k = j + 1;k < 6; ++k) 
+            {
+                if (igual(*(lista + j), *(lista + k))) 
+                {
+                    breakOut = true;
+                    break;
+                };
+            }
+            if (breakOut) { break; }
+        }
+        if (breakOut) {
+            breakOut = false;
+            continue;
+        }
+        for (int i = 0; i < 5; i++) 
+        {
+            s += menor(*(lista + i), *(lista + i + 1));
+        }
+        if (s == 5) { c++; };
+        if (s == 0) { d++; };
+    }
+
+    std::cout << "ordem crescente: " << c << std::endl;
+    std::cout << "ordem decrescente: " << d << std::endl;
+    std::cout << "total: " << c + d << std::endl;
+    return 0;
+}
